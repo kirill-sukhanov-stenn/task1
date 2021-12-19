1f863b9d9780328287d7853a85cf4be1b3e9fd37
Kirill Sukhanov <kirill.sukhanov@stenn.com>
2021-12-19 20:22:23 +0300

    Почистил код

--- remake1_task1.py
+++ remake1_task1.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
+
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 class Task1AddGroup(unittest.TestCase):
     def setUp(self):
@@ -40,20 +38,6 @@ class Task1AddGroup(unittest.TestCase):
         driver.find_element_by_link_text("groups").click()
         driver.find_element_by_link_text("Logout").click()
 
-
-
-    
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-
-    
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
