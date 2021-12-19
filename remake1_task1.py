9a100641e9d33e8953fed0d2fa72c5d1b4dfc094
Kirill Sukhanov <kirill.sukhanov@stenn.com>
2021-12-19 22:50:31 +0300

    Добавил комментарии

--- remake1_task1.py
+++ remake1_task1.py
@@ -1,45 +1,49 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
 
 class Task1AddGroup(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
     
     def test_task1_add_group(self):
-        driver = self.driver
-        driver.get("http://localhost/addressbook/addressbook/")
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-        driver.find_element_by_link_text("groups").click()
-        driver.find_element_by_xpath("//div[@id='content']/form/input[4]").click()
-        driver.find_element_by_name("group_name").click()
-        driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("bjkknklm")
-        driver.find_element_by_name("group_header").click()
-        driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("jbjnkjnm")
-        driver.find_element_by_name("group_footer").click()
-        driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("jjnlkm,.")
-        driver.find_element_by_name("submit").click()
-        driver.find_element_by_link_text("groups").click()
-        driver.find_element_by_link_text("Logout").click()
+        wd = self.wd
+        # open home page
+        wd.get("http://localhost/addressbook/addressbook/")
+        # login
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+        # open groups page
+        wd.find_element_by_link_text("groups").click()
+        # init group creation
+        wd.find_element_by_xpath("//div[@id='content']/form/input[4]").click()
+        # fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("bjkknklm")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("jbjnkjnm")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("jjnlkm,.")
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        # return to groups page
+        wd.find_element_by_link_text("groups").click()
+        # logout
+        wd.find_element_by_link_text("Logout").click()
 
     def tearDown(self):
-        self.driver.quit()
+        self.wd.quit()
         self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
