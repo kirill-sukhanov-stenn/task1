6423a900786b50723ed032feb1b63ecd20cda889
Kirill Sukhanov <kirill.sukhanov@stenn.com>
2021-12-26 17:13:09 +0300

    Создал класс Менеджер

--- fixture/application.py
+++ fixture/application.py
@@ -1,14 +1,12 @@
 from selenium import webdriver
-from selenium.webdriver.support.select import Select
-from fixture.session import SessionHelper
-from fixture.contact import ContactHelper
+from fixture.manager import Manager
 
 class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-        self.session = SessionHelper(self)
-        self.contact = ContactHelper(self)
+        self.manager = Manager(self)
+
 
     def open_home_page(self):
         wd = self.wd
