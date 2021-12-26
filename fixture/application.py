529f15c13fbee91fbf504b912234bcc38e4614ec
Kirill Sukhanov <kirill.sukhanov@stenn.com>
2021-12-26 19:34:29 +0300

    Добавление тестов с группами

--- fixture/application.py
+++ fixture/application.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
-from fixture.manager import Manager
+from fixture.session import SessionHelper
+from fixture.contact import ContactHelper
+from fixture.group import GroupHelper
 
 class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
-        self.manager = Manager(self)
+        self.session = SessionHelper(self)
+        self.contact = ContactHelper(self)
+        self.group = GroupHelper(self)
 
 
     def open_home_page(self):
