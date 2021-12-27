b4795f1f80be215095fa25e9274c318150496624
Kirill Sukhanov <kirill.sukhanov@stenn.com>
2021-12-27 15:16:28 +0300

    Создание теста - удаления группы

--- fixture/group.py
+++ fixture/group.py
@@ -1,9 +1,8 @@
 
-
 class GroupHelper:
 
     def __init__(self, app):
-        self.app=app
+        self.app =app
 
 
     def open_groups_page(self):
@@ -25,6 +24,15 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("group page").click()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
 
     def return_to_groups_page(self):
         wd = self.app.wd
