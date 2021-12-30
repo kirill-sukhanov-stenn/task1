dac4512c711763db56b59c5cea6b3721707c40e0
Kirill Sukhanov <kirill.sukhanov@stenn.com>
2021-12-30 17:05:16 +0300

    Создание тестов таблицы контактов

--- test/test_update_contact.py
+++ test/test_update_contact.py
@@ -16,36 +16,3 @@ def test_update_first_contact(app):
                 phone_2="jjgjhknkjnm,", notes_contact="fduygihjhbjlnljk"))
     app.session.logout()
 
-
-def test_details(app):
-    app.session.login(username="admin", password="secret")
-    app.contact.test_details()
-    app.session.logout()
-
-
-def test_print_details(app):
-    app.session.login(username="admin", password="secret")
-    app.contact.test_print_details()
-    app.session.logout()
-
-
-def test_details_modify(app):
-    app.session.login(username="admin", password="secret")
-    app.contact.update_first_contact(Contact(first_name="trrdytfyuhgjkbhk", middle_name="gvjhbkjbnjknlk", last_name="hvbjbkjnlknm",
-                nick="gfuhbkhjbkljnlm", title_contact="hfuygujhkl",
-                company_contact="giuhjnllk", contact_address="uyfuyighklnmlknkjbn",
-                home_contact="gfhgghjkh",
-                mobile_phone="456778789789", work_phone="645768", fax_phone="46756879890",
-                email_com="lvcgjhbjkbnm,n@bvhfgh.com", email2="yghjhjklk@gvhjbhj.com",
-                home_page="gfjhbgkjnkjln",
-                b_day="14", b_month="October",
-                b_year="1234", a_day="17", a_month="November", a_year="1989",
-                address_2="tdcghvbkhjbnkjn",
-                phone_2="jjgjhknkjnm,", notes_contact="fduygihjhbjlnljk"))
-    app.session.logout()
-
-
-def test_vCard(app):
-    app.session.login(username="admin", password="secret")
-    app.contact.test_vCard()
-    app.session.logout()
