1792beda668d231f46590f5d1b12882387e87ada
Kirill Sukhanov <kirill.sukhanov@stenn.com>
2022-01-23 16:42:42 +0300

    Доработка 14 задания

--- test/test_home_page.py
+++ test/test_home_page.py
@@ -6,10 +6,10 @@ def test_random_contact_home_page(app):
     index = randrange(len(old_contacts))
     contact_from_home_page = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.first_name == firstname_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.first_name == contact_from_edit_page.first_name
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.last_name == lastname_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.contact_address == address_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.last_name == contact_from_edit_page.last_name
+    assert contact_from_home_page.contact_address == contact_from_edit_page.contact_address
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
 
 def clear(s):
@@ -21,16 +21,6 @@ def merge_phones_like_on_home_page(contact):
                                                            [contact.home_contact,
                                                             contact.mobile_phone, contact.work_phone,
                                                             contact.phone_2]))))
-
-def firstname_like_on_home_page(contact):
-    return contact.first_name
-
-def lastname_like_on_home_page(contact):
-    return contact.last_name
-
-def address_like_on_home_page(contact):
-    return contact.contact_address
-
 
 def merge_emails_like_on_home_page(contact):
         return "\n".join(filter(lambda x: x != "", filter(lambda x: x is not None, [contact.email_com,
