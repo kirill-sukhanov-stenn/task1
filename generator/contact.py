c9ad9167ded72be379e2791b489aea44e93e3d21
Kirill Sukhanov <kirill.sukhanov@stenn.com>
2022-01-23 19:00:59 +0300

    Реализована загрузка тестовых данных из файла и параметризация. Задание 19

--- generator/contact.py
+++ generator/contact.py
@@ -58,5 +58,5 @@ testdata = [Contact(first_name="", middle_name="", last_name="",
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    jsonpickle.set_encoder_options("json", indent = 2)
+    jsonpickle.set_encoder_options("json", indent=2)
     out.write(jsonpickle.encode(testdata))
