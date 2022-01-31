16073c77a541878a7b8ec2f4f67e27e318a866d5
Kirill Sukhanov <kirill.sukhanov@stenn.com>
2022-01-31 14:15:32 +0300

    ОРМ

---
+++ fixture/db.py
@@ -0,0 +1,42 @@
+import pymysql.cursors
+from model.group import Group
+from model.contact import Contact
+
+class DbFixture:
+
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = pymysql.connect(host=host, database=name, user=user, password=password, autocommit=True)
+
+    def get_group_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, name, header, footer) = row
+                list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id,  lastname, firstname, address, home, mobile, work, email, email2, email3 from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, lastname, firstname,  address, home, mobile, work, email, email2, email3) = row
+                list.append(Contact(id=str(id), last_name=lastname, first_name=firstname, contact_address=address,
+                                    home_contact=home,
+                                  mobile_phone=mobile, work_phone=work, email_com=email, email2=email2, email3=email3))
+        finally:
+            cursor.close()
+        return list
+
+    def destroy(self):
+        self.connection.close()
+
---
+++ test/test_db_matches_ui.py
@@ -0,0 +1,17 @@
+from model.group import Group
+from model.contact import Contact
+
+
+def test_group_list(app, db):
+    ui_list = app.group.get_group_list()
+    def clean(group):
+        return Group(id=group.id, name=group.name.strip())
+    db_list = map(clean, db.get_group_list())
+    assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+def test_contact_list(app, db):
+    ui_list = app.contact.get_contact_list()
+    def clean(contact):
+        return Contact(id=contact.id, first_name=contact.first_name.strip(), last_name=contact.last_name.strip())
+    db_list = map(clean, db.get_contact_list())
+    assert sorted(ui_list, key=Contact.id_or_max) == sorted(db_list, key=Contact.id_or_max)
