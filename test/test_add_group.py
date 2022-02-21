c93d530519ce20f4fe65e496570a954d8f78f72c
Kirill Sukhanov <kirill.sukhanov@stenn.com>
2022-02-21 22:05:57 +0300

    Allure fix

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,15 +1,15 @@
 from model.group import Group
-import pytest
+import allure
 
 
 
 def test_add_group(app, db, check_ui, json_groups):
     group = json_groups
-    with pytest.allure.step('Given a group list'):
+    with allure.step('Given a group list'):
         old_groups = db.get_group_list()
-    with pytest.allure.step('When I add a group %s to the list' % group):
+    with allure.step('When I add a group %s to the list' % group):
         app.group.create(group)
-    with pytest.allure.step('Then the new group list is equal to the old list with the added group'):
+    with allure.step('Then the new group list is equal to the old list with the added group'):
         new_groups = db.get_group_list()
         old_groups.append(group)
         assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
